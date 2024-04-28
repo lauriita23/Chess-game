ec8e07ace7a15e7f2fc919b3e4f102e46da3d2a6
Laura <lauradelacruzcirujano@gmail.com>
2024-04-28 18:30:23 +0200

    admin modificado

--- mychess/models/admin.py
+++ mychess/models/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from models.models import ChessGame, ChessMove, Player
+
+@admin.register(ChessGame)
+class ChessGameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'start_time', 'end_time', 'timeControl', 'whitePlayer', 'blackPlayer', 'winner')
+    list_filter = ('status', 'timeControl', 'winner')
+    search_fields = ('id', 'status', 'timeControl', 'winner')
+    ordering = ('-start_time',)
+
+@admin.register(ChessMove)
+class ChessMoveAdmin(admin.ModelAdmin):
+    list_display = ('id', 'game', 'player', 'move_from', 'move_to')
+    list_filter = ('player',)
+    search_fields = ('id', 'game', 'player', 'move_from', 'move_to')
+    ordering = ('-id',)
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'rating')
+    search_fields = ('username', )
+    ordering = ('username',)
