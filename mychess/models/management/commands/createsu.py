10a08f3744d24344921fde47fc4a702710955661
Ana <anagllaneza.uam@gmail.com>
2024-05-02 09:33:30 +0200

    modificacion build3

---
+++ mychess/models/management/commands/createsu.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Command(BaseCommand):
+    help = 'Creates a superuser.'
+    
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='alumnodb').exists():
+            User.objects.create_superuser(
+                username=getenv('USERNAME'),
+                password=getenv('PASSWORD'),
+            )
+        print('Superuser has been created.')
